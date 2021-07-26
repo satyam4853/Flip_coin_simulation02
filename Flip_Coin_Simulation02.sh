699deb538d2f95ed6266bd2842af117266b195b2
Your Name <you@example.com>
2021-07-26 15:21:00 +0530

    multiple flipping

--- Flip_Coin_Simulation02.sh
+++ Flip_Coin_Simulation02.sh
@@ -2,8 +2,25 @@ echo "welcome to simulator problem 02 of coin "
 #!/bin/bash -x
 ((flip=RANDOM%2))
 
-if [ $flip -eq 0 ] ; then
+if [ $flip -eq 0 ] 
+then
         echo Heads
 else
         echo Tails
-fi 
+fi
+read -p "Enter number of times you want to flip the coin : " flip_number
+	
+
+	while [ $flip_number -ne 0 ] ; do
+		((flip=RANDOM%2))
+		if [ $flip -eq 0 ] ; then
+			((heads_count+=1))
+		else
+			((tails_count+=1))
+		fi
+		((flip_number-=1))
+	done
+	
+
+	echo Heads Score : $heads_count --- Tails Score : $tails_count
+
