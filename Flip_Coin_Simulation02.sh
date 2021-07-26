9cf042cdef39f0212d544285b2a63a6a914a28d0
Your Name <you@example.com>
2021-07-26 15:28:36 +0530

    check ow many times heads or tails wins by ehat margine

--- Flip_Coin_Simulation02.sh
+++ Flip_Coin_Simulation02.sh
@@ -23,4 +23,28 @@ read -p "Enter number of times you want to flip the coin : " flip_number
 	
 
 	echo Heads Score : $heads_count --- Tails Score : $tails_count
+heads_count=0
+	tails_count=0
+	while [ $heads_count -ne 21 -a $tails_count -ne 21 ] ; do
+		((flip=RANDOM%2))
+		if [ $flip -eq 0 ] ; then
+			((heads_count+=1))
+		else
+			((tails_count+=1))
+		fi
+	done
+	
+
+	if [ $heads_count -eq 21 ] ; then
+		echo Heads WON!
+		echo Head Won by $(($heads_count-$tails_count))
+	elif [ $tails_count -eq 21 ] ; then
+		echo Tails WON!
+	        echo Tails Won by $(($tails_count-$heads_count))
+	fi
+	
+
+
+
+
 
